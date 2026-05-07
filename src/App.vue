df2448ff7ffac084ebe60f32ddd92e16fae5af66
rumosky <rumosky@163.com>
2026-05-07 21:35:29 +0800

    修复工作流打包错误问题

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref, onMounted, nextTick, computed, watch } from 'vue'
 import { invoke } from '@tauri-apps/api/core'
 import { getVersion } from '@tauri-apps/api/app'
 import { open } from '@tauri-apps/plugin-dialog'
-// import { ElMessage, ElMessageBox } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 import { marked } from 'marked'
 
 interface Message {
